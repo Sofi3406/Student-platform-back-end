e24ebfb866d8f822529af8eb40a91944e99bbc0e
Sofiya Yasin <sofiyasin190@gmail.com>
2025-08-07 20:23:05 -0700

    feat: add comment on userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,8 @@
+// userRoutes
 const router = require('express').Router();
 const { getUsersByRole } = require('../controllers/userController');
 const verifyToken = require('../middleware/authMiddleware');
 const permit = require('../middleware/roleMiddleware');
-
-// Only admin can view all users by role
 router.get('/', verifyToken, permit('admin'), getUsersByRole);
 
 module.exports = router;
